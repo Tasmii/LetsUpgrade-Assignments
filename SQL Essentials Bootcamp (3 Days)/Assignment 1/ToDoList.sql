79960b346adde06e19e0e38f1ac15b5af468ca3f
Tasmiya Ilahi <91000777+Tasmii@users.noreply.github.com>
2023-06-27 21:50:52 +0530

    Update ToDoList.sql

--- SQL Essentials Bootcamp (3 Days)/Assignment 1/ToDoList.sql
+++ SQL Essentials Bootcamp (3 Days)/Assignment 1/ToDoList.sql
@@ -1,3 +1,7 @@
+--Question:
+--"Using SQL, create a database for a simple todo list program that has the following columns: task_id (primary key), task_name, description, and is_completed."
+
+--Answer:
 CREATE TABLE todo_list (
   task_id INT PRIMARY KEY,
   task_name VARCHAR(255),
